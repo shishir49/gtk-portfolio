c988beb62f2751c2d6ddfbf633991154f4f406b0
shishir49 <ks.azim@yahoo.com>
2022-11-02 19:01:26 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GTK</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="Asset/css/style.css">
+    <link rel="stylesheet" href="Asset/css/responsive.css">
+
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js" integrity="sha512-naukR7I+Nk6gp7p5TMA4ycgfxaZBJ7MO5iC3Fp6ySQyKFHOGfpkSZkYVWV5R7u7cfAicxanwYQ5D1e17EfJcMA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+<body>
+    <div class="header">
+        <div class="logo">
+            <img src="Asset/images/logo.png" alt="Logo" srcset="">
+        </div>
+        <div class="menu">
+            <a href="#product">Watch a Demo</a>
+            <a href="#pricing">Price</a>
+            <a href="#footer">Contact Us</a>
+        </div>
+    </div>
+    <div class="contents">
+        <div class="content-segment product" id="product">
+            <div class="features">
+                <div class="content-headline">
+                    <h2>Check The Most Advanced MCQ Test Software</h2>
+                </div>
+                <div class="content-body">
+                    <p class="bullets"><span class="check-icon"><i class="fa-solid fa-check"></i></span> Create, Download Unlimited Tests !</p>
+                    <p class="bullets"><span class="check-icon"><i class="fa-solid fa-check"></i></span> Create Entire Test in 2 submit clicks !</p>
+                    <p class="bullets"><span class="check-icon"><i class="fa-solid fa-check"></i></span> Set up Time and Force submission</p>
+                    <p class="bullets"><span class="check-icon"><i class="fa-solid fa-check"></i></span> Negetive Scoring</p>
+                    <p class="bullets"><span class="check-icon"><i class="fa-solid fa-check"></i></span> Instant Result publication</p>
+                    <p class="bullets"><span class="check-icon"><i class="fa-solid fa-check"></i></span> Student Ranking based on their Score</p>
+                    <p class="bullets"><span class="check-icon"><i class="fa-solid fa-check"></i></span> Student Test Results with details</p>
+                </div>
+            </div>
+            <a  class="demo" href="https://mathematoz.com">
+            </a>
+        </div>
+        <div class="content-segment pricing" id="pricing">
+            <div class="content-headline center">
+                <h2>Pricing Table</h2>
+            </div>
+            <div class="content-body">
+                <table class="custom-table two-third">
+                    <thead>
+                        <th>Features</th>
+                        <th>Economy</th>
+                        <th>Business</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Unlimited Test</td>
+                            <td>YES</td>
+                            <td>YES</td>
+                        </tr>
+                        <tr>
+                            <td>Downloading result (Excel, PDF, etc)</td>
+                            <td>YES</td>
+                            <td>YES</td>
+                        </tr>
+                        <tr>
+                            <td>Access Student Result</td>
+                            <td>YES</td>
+                            <td>YES</td>
+                        </tr>
+                        <tr>
+                            <td>Training</td>
+                            <td>YES (1 Week)</td>
+                            <td>YES (1 Week)</td>
+                        </tr>
+                        <tr>
+                            <td>Database</td>
+                            <td>NO</td>
+                            <td>YES</td>
+                        </tr>
+                        <tr>
+                            <td>Free Support</td>
+                            <td>1 Month</td>
+                            <td>3 Months</td>
+                        </tr>
+                        <tr>
+                            <td>Hosting</td>
+                            <td>Cpanel</td>
+                            <td>Cpanel or Cloud</td>
+                        </tr>
+                        <tr>
+                            <td>Yearly Chrage (BDT)</td>
+                            <td>Depends on Storage Capacity</td>
+                            <td>Depends on Hosting provider and Storage Capacity</td>
+                        </tr>
+                        <tr>
+                            <td>Price (BDT)</td>
+                            <td>20K</td>
+                            <td>50K</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <!-- <div class="contact-form" id="contact-form">
+            <div class="content-headline">
+                <h2>Ask a Question</h2>
+            </div>
+            <div class="content-body">
+                <form action="" method="post">
+                    <label for="">Name</label>
+                    <input type="text">
+                    <br>
+                    <label for="">Mobile/Phone</label>
+                    <input type="text">
+                    <br>
+                    <label for="">Subject</label>
+                    <select name="" id="">
+                        <option value="">Pick an Option</option>
+                        <option value="">Product Details</option>
+                        <option value="">Payment Details</option>
+                    </select>
+                    <br>
+                    <label for="">Details</label>
+                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                    <br>
+                    <button>Submit</button>
+                </form>
+            </div>
+        </div> -->
+    </div>
+    <div class="footer" id="footer">
+        <h2>Contact Us</h2>
+        <p><i class="fa-solid fa-phone-volume"></i> +880 1773-134268, +88 01781410322</p>
+        <p><i class="fa-solid fa-envelope"></i> gtkgroup1956@gmail.com</p>
+        <p><i class="fa-solid fa-location-dot"></i> House -15/ Sector -18/ Uttara / Dhaka-1230</p>
+    </div>
+</body>
+</html>
